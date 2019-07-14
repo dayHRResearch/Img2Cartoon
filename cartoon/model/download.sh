6f7347517586de5f66cba1637a6e6e26dcb8c18d
Lornatang <shiyipaisizuo@163.com>
2019-07-14 11:53:16 +0800

    Conversion model method

--- cartoon/model/download.sh
+++ cartoon/model/download.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+cd model/download.sh
+
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hayao_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hosoda_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Paprika_net_G_float.pth	
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Shinkai_net_G_float.pth
+
+mv model/Hayao_net_G_float.pth model/hayao.pth
+mv model/Hosoda_net_G_float.pth model/hosoda.pth
+mv model/Paprika_net_G_float.pth model/paprika.pth
+mv model/Shinkai_net_G_float.pth model/shinkai.pth
