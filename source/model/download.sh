e7901026b3fe785737fcb8d4847e4788ed8f0041
changyu_liu <changyu_liu@dayhr.com>
2019-07-15 15:58:26 +0800

    Update download.sh

--- source/model/download.sh
+++ source/model/download.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-cd source
-
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hayao_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hosoda_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Paprika_net_G_float.pth	
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Shinkai_net_G_float.pth
 
-mv Hayao_net_G_float.pth model/hayao.pth
-mv Hosoda_net_G_float.pth model/hosoda.pth
-mv Paprika_net_G_float.pth model/paprika.pth
-mv Shinkai_net_G_float.pth model/shinkai.pth
+mv Hayao_net_G_float.pth hayao.pth
+mv Hosoda_net_G_float.pth hosoda.pth
+mv Paprika_net_G_float.pth paprika.pth
+mv Shinkai_net_G_float.pth shinkai.pth
