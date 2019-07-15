e58ee972b1d1ce053978fc21cba25ff71785d1d1
changyu_liu <changyu_liu@dayhr.com>
2019-07-15 15:57:21 +0800

    Update download.sh

--- source/model/download.sh
+++ source/model/download.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+cd source
+
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hayao_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Hosoda_net_G_float.pth
 wget -c http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/Paprika_net_G_float.pth	
