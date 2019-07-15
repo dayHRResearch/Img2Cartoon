dce4f846f601e3938f70a1f9b5d0964d0c723afb
changyu_liu <changyu_liu@dayhr.com>
2019-07-15 10:25:34 +0800

    Update test.py

--- source/test.py
+++ source/test.py
@@ -30,7 +30,7 @@
 import argparse
 import os
 import sys
-sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0], '../..'))
+sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0], '../'))
 
 import cv2
 import numpy as np
